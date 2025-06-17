50a9425b019e050fca5a85f99882acfbef2643b5
abdullahkhrisstoo <77031579+abdullahkhrisstoo@users.noreply.github.com>
2025-06-18 01:51:37 +0300

    Update FileWriterGenerator.cs

--- FileWriterGenerator.cs
+++ FileWriterGenerator.cs
@@ -1,1 +1,31 @@
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seagull.FrameWork.Repository.ModelCodeGenerator
+{
+    public class FileWriterGenerator
+    {
+        public static async Task WriteToFileAsync(string filePath, string content)
+        {
+            try
+            {
+                var directory = Path.GetDirectoryName(filePath);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                await File.WriteAllTextAsync(filePath, content);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
